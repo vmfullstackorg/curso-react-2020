426102aa1f3dabe05886f2e1ab532ba75a8b9b71
vmfullstack <vmfullstack@outlook.com>
2021-07-06 16:15:01 -0300

    Comportamentos alterando em tempo real

--- iniciando-com-react/src/App.js
+++ iniciando-com-react/src/App.js
@@ -3,9 +3,17 @@ class App extends React.Component {
   state = {
     nome: 'Reynaldo Santos'
   }
+
+  modificarNome = (event) => {
+    let txtnome = event.target.value;
+    this.setState({ nome: txtnome })
+  }
   render() {
     return (
-      <h2>Hello {this.state.nome}</h2>
+      <React.Fragment>
+        <input type="text" value={this.state.nome} onChange={this.modificarNome} />
+        <h2>Hello {this.state.nome}</h2>
+      </React.Fragment>
     )
   }
 }
